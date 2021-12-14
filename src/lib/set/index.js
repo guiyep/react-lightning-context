308dcf8d69e977fd368144435a38e43680bfd05d
guiyep <guiyep@gmail.com>
2021-12-14 16:53:11 +1000

    remove get lodash

---
+++ src/lib/set/index.js
@@ -0,0 +1,14 @@
+export const set = (obj, path, value) => {
+  var schema = obj;
+  var pList = path.split('.');
+  var len = pList.length;
+  for (var i = 0; i < len - 1; i++) {
+    var elem = pList[i];
+    if (!schema[elem]) schema[elem] = {};
+    schema = schema[elem];
+  }
+
+  schema[pList[len - 1]] = value;
+
+  return obj;
+};
---
+++ src/lib/set/test.js
@@ -0,0 +1,39 @@
+import { set } from './index';
+
+const state = {
+  valA: {
+    valB: {
+      valC: 222,
+    },
+  },
+};
+
+describe('set', () => {
+  test('to set value', () => {
+    expect(set(state, 'valA.valB.valC', 555)).toEqual({
+      valA: {
+        valB: {
+          valC: 555,
+        },
+      },
+    });
+
+    expect(set(state, 'valA.valB', 555)).toEqual({
+      valA: {
+        valB: 555,
+      },
+    });
+
+    expect(set(state, 'valA', 555)).toEqual({
+      valA: 555,
+    });
+  });
+
+  test('to throw', () => {
+    try {
+      set(state, '');
+    } catch (e) {
+      expect(e.message).toEqual('path need to be present');
+    }
+  });
+});
